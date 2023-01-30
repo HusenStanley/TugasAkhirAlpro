ed5109705358ec76f1d1f65d2e12e52ebe643aa2
HusenStanley <stanleydaniel21222330@gmail.com>
2023-01-30 11:20:14 +0700

    Menambah Tabel dan Variable

--- tugasbesarstanleyhusen.py
+++ tugasbesarstanleyhusen.py
@@ -1,4 +1,4 @@
-my_file = open("ListMenu.txt", "r") #r untuk membaca file txt
+my_file = open("ListMenu.txt", "r") #r untuk membaca file txt #my_file berfungsi untuk variabel membuka file
 count = 0
 
 listmakanan = []
@@ -7,13 +7,13 @@ listsnack = []
 
 while True:
     count += 1  
-    data = my_file.readline()
+    data = my_file.readline() #readline untuk membaca isi file perbaris dan akan mengembalikan nilai berupa list
     if not data:
         break
     if count == 1 :
-        listmakanan = data.rstrip().split(",")
+        listmakanan = data.rstrip().split(",") #rstrip untuk menghilangkan enter
     elif count == 2 :
-        listminuman = data.rstrip().split(",")
+        listminuman = data.rstrip().split(",") #split untuk memberi jarak
     elif count == 3 :
         listsnack = data.rstrip().split(",")
 
@@ -21,35 +21,64 @@ def lihatData():
     print("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|")
     print("|< ================= * Daftar Menu * ================ >|")
     print("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|")
-    data = zip(listmakanan, listminuman, listsnack)
-    print ("|{:<5}|{:<15}|{:<15}|{:<16}|".format('No','Makanan','Minuman','Snack'))
+    data = zip(listmakanan, listminuman, listsnack) #zip untuk menggabungkan menjadi 1 list
+    print ("|{:<5}|{:<15}|{:<15}|{:<16}|".format('  No','    Makanan','    Minuman','     Snack'))
     print("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|")
-    for index, v in enumerate(data):
+    for index, v in enumerate(data): #enumerate untuk mendapatkan index dari list data
         makanan, minuman, snack = v
         print ("|{:<5}|{:<15}|{:<15}|{:<16}|".format( index+1, makanan, minuman, snack))
 
 def tambahData(makanan,minuman,snack):
-    listmakanan.append(makanan)
+    listmakanan.append(makanan) #append untuk menambah data
     listminuman.append(minuman)
     listsnack.append(snack)
+    stringmakanan = ",".join(listmakanan)
+    stringminuman = ",".join(listminuman)
+    stringsnack = ",".join(listsnack) 
+    my_file = open("ListMenu.txt", "w") #w untuk membuka txt dengan fungsi write pada file
+    my_file.write(stringmakanan + "\n") #\n untuk enter
+    my_file.write(stringminuman + "\n")
+    my_file.write(stringsnack)
+    my_file.close()
 
 def hapusData(hapus):
-    listmakanan.pop(hapus - 1)
+    listmakanan.pop(hapus - 1) #Pop Berfungsi untuk menghapus
     listminuman.pop(hapus - 1)
     listsnack.pop(hapus - 1)
+    stringmakanan = ",".join(listmakanan)
+    stringminuman = ",".join(listminuman)
+    stringsnack = ",".join(listsnack) 
+    my_file = open("ListMenu.txt", "w") #my_file berfungsi untuk membuka file  #w untuk membuka txt dengan fungsi write pada file
+    my_file.write(stringmakanan + "\n")
+    my_file.write(stringminuman + "\n")
+    my_file.write(stringsnack)
+    my_file.close()
     
 def ubahData(ubah, makanan, minuman, snack):
     listmakanan[ubah - 1] = makanan
     listminuman[ubah - 1] = minuman
     listsnack[ubah - 1] = snack
+    stringmakanan = ",".join(listmakanan)
+    stringminuman = ",".join(listminuman)
+    stringsnack = ",".join(listsnack) 
+    my_file = open("ListMenu.txt", "w") #my_file berfungsi untuk membuka file #w untuk membuka txt dengan fungsi write pada file
+    my_file.write(stringmakanan + "\n")
+    my_file.write(stringminuman + "\n")
+    my_file.write(stringsnack)
+    my_file.close()
     
-listMenu = ["1.Menambah Menu", "2.Menampilkan Menu", "3.Mengubah Menu" ,"4.Menghapus Menu"]
+    
+listMenu =["  |1. Menambah Menu     |", "  |2. Menampilkan Menu  |", "  |3. Mengubah Menu     |" ,"  |4. Menghapus Menu    |"]
 
 while True :
-    print("Daftar Menu :")
+    print("  |=====================|")
+    print("  |<   * List Menu *   >|")
+    print("  |=====================|")
     for x in listMenu :
         print(x)
-    print("Pilih Menu :")
+    print("  |=====================|")
+    print("  |    * Pilih Menu *   |")
+    print("  |=====================|")
     menu =int(input())
     if menu == 1 :
         print("Masukkan makanan :")
@@ -62,7 +91,7 @@ while True :
     elif menu == 2 :
         lihatData()
     elif menu == 3 :
-        print("Data Yang Akan Diubah")
+        print("Menu Yang Akan Diubah")
         ubah =int(input())
         print("Masukkan makanan :")
         makanan =str(input())
@@ -72,10 +101,12 @@ while True :
         snack =str(input())
         ubahData(ubah, makanan, minuman, snack)
     elif menu == 4 :
-        print("Data Yang Akan Dihapus")
+        print("Menu Yang Akan Dihapus")
         hapus =int(input())
         hapusData(hapus)
     else :
         break
+    print("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|")
+
   
    
